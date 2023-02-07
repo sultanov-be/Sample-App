3f157dd5f8cc0e1bfb2222676039adfcb746ab1b
Bektur <yasultanovbektur@yandex.ru>
2023-02-07 10:52:25 +0600

    Add MainFragment, NavController.

---
+++ app/src/main/java/com/example/sampleapp/ui/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.example.sampleapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.sampleapp.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+    private val viewModel by viewModels<MainViewModel>()
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel
+    }
+
+}
