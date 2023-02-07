946dad983e1966974f156ef2c9a38988001a4f51
Bektur <yasultanovbektur@yandex.ru>
2023-02-07 11:38:28 +0600

    Prepare DAO.

--- app/src/main/java/com/example/sampleapp/database/StudentDatabaseDao.kt
+++ app/src/main/java/com/example/sampleapp/database/StudentDatabaseDao.kt
@@ -1,4 +1,22 @@
 package com.example.sampleapp.database
 
-class StudentDatabaseDao {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface StudentDatabaseDao {
+    @Insert
+    fun insert(student: Student)
+
+    @Update
+    fun update(student: Student)
+
+    @Query("SELECT * from student_list_table WHERE studentId = :key")
+    fun get(key: PrimaryKey): Student
+
+    @Query("DELETE FROM student_list_table")
+    fun clear()
+
+    @Query("SELECT * FROM student_list_table ORDER BY studentId DESC")
+    fun getAllNights(): LiveData<List<Student>>
 }
